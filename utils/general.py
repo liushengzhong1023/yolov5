e3db4df950f14ceadb14218700c48402afd158ce
liushengzhong <sl29@illinois.edu>
2021-05-10 18:04:47 -0700

    first commit of partial image infernece with yolov5

--- utils/general.py
+++ utils/general.py
@@ -458,7 +458,8 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
        THe NMS on each image is performed sequentially.
 
     Input:
-        prediction: output of the model,
+        prediction: output of the model, [batch_size, #boxes, 80+5]
+        85: [x_c, y_c, w, h, objectness, class_conf_80]
 
     Returns:
          list of detections, on (n,6) tensor per image [xyxy, conf, cls]
@@ -506,7 +507,8 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
         if multi_label:
             i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
             x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
-        else:  # best class only
+        else:
+            # best class only
             conf, j = x[:, 5:].max(1, keepdim=True)
             x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
 
