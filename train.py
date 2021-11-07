1702eba51b7f2ab1cf73403f6a2f688ac20d6377
liushengzhong <sl29@illinois.edu>
2021-11-07 00:17:04 -0500

    add partially new post-processing (not working yet)

--- train.py
+++ train.py
@@ -456,13 +456,13 @@ def train(hyp, opt, device, tb_writer=None):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str,
-                        default='/home/shengzhong/offloading_scheduling/model/yolov5/yolov5m6.pt',
+                        default='/home/shengzhong/offloading_scheduling/model/yolov5/yolov5s.pt',
                         help='initial weights path')
     parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
     parser.add_argument('--data', type=str, default='data/visdrone.yaml', help='data.yaml path')
     parser.add_argument('--hyp', type=str, default='data/hyp.scratch.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=300)
-    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs')
+    parser.add_argument('--batch-size', type=int, default=24, help='total batch size for all GPUs')
     parser.add_argument('--img-size', nargs='+', type=int, default=[1280, 1280], help='[train, test] image sizes')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
     parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
